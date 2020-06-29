b6a6210e61a785e6fd747cddd3daa288903f9a56
benjimor <benji_moreau@hotmail.fr>
2020-06-29 15:40:52 +0200

    add url and views

--- marq_django/marq_django/urls.py
+++ marq_django/marq_django/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
+from marq_web.views import home, form, result
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^form/?$', form, name='form'),
+    url(r'^result/?$', result, name='result'),
+    url(r'', home, name='home')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- marq_web/views.py
+++ marq_web/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
+
+@require_http_methods(['GET'])
+def home(request):
+    return render(request, 'home.html')
+
+
+@require_http_methods(['GET'])
+def form(request):
+    return render(request, 'form.html')
+
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def result(request):
+    result = {}
+    return render(request, 'result.html', {'result': json.dumps(result)})
