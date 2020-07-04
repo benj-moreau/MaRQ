a956275d4ddccbfd1b3a0fe7f639b1696eec11ae
Manoé <manoe.kieffer@etu.univ-nantes.fr>
2020-07-04 17:43:24 +0200

    ajout des file input

--- marq_web/static/js/form.js
+++ marq_web/static/js/form.js
@@ -1,11 +1,23 @@
 var form = new Vue({
     el: '#form',
     data: {
+        test: '',
+        yourMapping: { name: 'Choose a mapping', file: 'default'},
         mappings: [
-            { name: 'exemple1.yml' },
-            { name: 'exemple2.yml' },
-            { name: 'exemple3.yml' },
-            { name: 'exemple4.yml' }
-        ]
-    }
+            { name: 'exemple1.yml', file: '' },
+            { name: 'exemple2.yml', file: '' },
+            { name: 'exemple3.yml', file: '' },
+            { name: 'exemple4.yml', file: '' }
+        ],
+        checkedMappings: []
+    },
+    methods: {
+        processFile(event) {
+            this.yourMapping.name = event.target.files[0].name
+            this.yourMapping.file = event.target.files[0]
+        },
+        addFile(event) {
+            this.mappings.push( {name: event.target.files[0].name, file:  event.target.files[0]} )
+        }
+}
 });
