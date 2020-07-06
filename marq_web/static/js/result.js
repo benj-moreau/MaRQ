907210becc8ebe7e35e3f28def49a842bc0e9e4a
Manoé <manoe.kieffer@etu.univ-nantes.fr>
2020-07-06 16:17:59 +0200

    création de bouton affichant les comparaisons correspondantes

--- marq_web/static/js/result.js
+++ marq_web/static/js/result.js
@@ -1,6 +1,16 @@
 var result = new Vue({
     el: '#result',
     data: {
-        marq_result: res
+        marq_result: res,
+        compSelected: 'default'
     },
+    watch: {
+        compSelected: function(val){
+            if(val = 'default'){
+                //do nothing
+            } else {
+                //calculer le graph et l'afficher
+            }
+        }
+    }
 });
