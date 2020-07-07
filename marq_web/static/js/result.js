0cb386088bab56933edc6667fa2435e44d527a0c
Manoé <manoe.kieffer@etu.univ-nantes.fr>
2020-07-07 15:36:47 +0200

    add a sorting fonction so that mappings are sorted by "Score"

--- marq_web/static/js/result.js
+++ marq_web/static/js/result.js
@@ -4,6 +4,24 @@ var result = new Vue({
         marq_result: res,
         compSelected: 'default'
     },
+    computed: {
+        //marq_results but in decreasing order
+        ordered_results: function(){
+            tempo = this.marq_result["comparaisons"]
+            result = []
+            while (tempo.length > 0) {
+                max = 0
+                for (i = 1; i < tempo.length; i++) {
+                    if (parseInt(tempo[i]["Score"]) > parseInt(tempo[max]["Score"])){
+                        max = i
+                    }
+                }
+                result.push(tempo[max])
+                tempo.splice(max, 1)
+            }
+            return result
+        }
+    },
     watch: {
         compSelected: function(val){
             if(val = 'default'){
